67e7430784bb15899a10fa5245928ea74439684d
Davide Davanzo <davide.davanzo@mail.polimi.it>
2021-03-05 21:28:32 +0100

    Added class Person in it.polimi.ingsw and class Temp in it.polimi.ingsw.temp (new package)

---
+++ src/main/java/it/polimi/ingsw/Person.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+public class Person {
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/temp/Temp.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.temp;
+
+public class Temp {
+    //OVERVIEW: this is a temporary class
+}
