6eb914a4134261de725f35835b4cfe5f9026f3c7
Davide Davanzo <davide.davanzo@mail.polimi.it>
2021-04-08 13:05:29 +0200

    Modified some features of Warehouse class, such as the possibility to have multiple extra shelves.

--- src/main/java/it/polimi/ingsw/model/playerboard/PlayerBoard.java
+++ src/main/java/it/polimi/ingsw/model/playerboard/PlayerBoard.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw.model.playerboard;
 
 public class PlayerBoard {
+
     private Warehouse warehouse;
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
 }
