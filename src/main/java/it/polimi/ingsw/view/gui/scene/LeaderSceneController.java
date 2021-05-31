50d3050b003be034ffa029dfdf84d0acbce41e5b
Dario58 <dario.delgaizo@mail.polimi.it>
2021-05-31 12:52:33 +0200

    leader scene

--- src/main/java/it/polimi/ingsw/view/gui/scene/LeaderSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene/LeaderSceneController.java
@@ -8,36 +8,47 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
-import java.io.File;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class LeaderSceneController implements GenericSceneController {
 
     private GuiView gui;
 
+    private ArrayList<LeaderCard> leaderCards;
+
     @FXML
-    private ImageView firstLeader;
+    private ImageView firstLeader = new ImageView();
     @FXML
-    private ImageView secondLeader;
+    private ImageView secondLeader = new ImageView();
     @FXML
-    private ImageView thirdLeader;
+    private ImageView thirdLeader = new ImageView();
     @FXML
-    private ImageView fourthLeader;
+    private ImageView fourthLeader = new ImageView();
     @FXML
     public HBox hbox;
 
     private ImageView[] images = {firstLeader, secondLeader, thirdLeader,fourthLeader};
+
+    public void initialize() {
+        setLeaderImages();
+    }
+
+    private void setLeaderImages() {
+        int last = 0;
+        for (ImageView imageView : images) {
+            Image img = new Image(getClass().getResourceAsStream("/images/cardsFront/" + leaderCards.get(last).getId() + ".png"));
+            imageView.setImage(img);
+            last++;
+        }
+    }
 
     @Override
     public void setGui(GuiView gui) {
         this.gui = gui;
     }
 
-    public void showLeaders(ArrayList<LeaderCard> leaderCards) {
-        for(int i = 0; i < leaderCards.size(); i++) {
-            images[i] = new ImageView(new Image(getClass().getResource("/images/cardsFront" + leaderCards.get(i).getId() + ".png").toString()));
-            hbox.getChildren().add(images[i]);
-        }
+    public void setLeaderCards(ArrayList<LeaderCard> leaderCards) {
+        this.leaderCards = leaderCards;
     }
 }
